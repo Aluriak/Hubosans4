2fcccc1212ed35852b22e478643d78f903ec385f
Aluriak <lucas.bourneuf@laposte.net>
2012-10-20 19:30:27 +0200

    Initialisation du module de moteur de jeu

---
+++ modules/moteur/moteur.h
@@ -0,0 +1,23 @@
+#ifndef MOTEUR_H_INCLUDED
+#define MOTEUR_H_INCLUDED
+
+/*
+ * BIBLIOTHEQUES
+ */
+#include <stdio.h>
+#include <stdlib.h>
+
+
+/*
+ * PREDECLARATIONS
+ */
+
+
+
+/*
+ * PROTOTYPES
+ */
+
+
+
+#endif
