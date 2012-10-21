eaa00db362581e74d72e5517828016032c62cdbf
Aluriak <lucas.bourneuf@laposte.net>
2012-10-21 11:51:17 +0200

    Ne définit plus les structures du moteur de jeu

--- main.h
+++ main.h
@@ -20,37 +20,6 @@
 /*
  * DECLARATIONS
  */
-// il existe 126 id de joueurs. Soit 126 joueurs au max 
-// les négatifs sont utilisés pour l'absence de joueur
-typedef short idJ;
-
-
-// Coordonnées entières
-typedef struct {
-    int x;
-    int y;
-} coord;
-
-
-// Case du plateau de jeu
-typedef struct {
-    coord crd; // coordonnées de la case dans le jeu
-    idJ joueurPieceCreuse; // joueur contrôlant la pièce creuse, ou -1
-    idJ joueurPiecePleine; // joueur contrôlant la pièce pleine, ou -1
-} t_case;
-
-
-/* 
- * STRUCT: T_CASE 
- */
-// initialisation de la structure
-void t_case_init(t_case* t_case, int x, int y) {
-    t_case->crd.x = x;
-    t_case->crd.y = y;
-    t_case->joueurPieceCreuse = -1;
-    t_case->joueurPiecePleine = -1;
-    t_case->p = VIDE;
-}
 
 
 
