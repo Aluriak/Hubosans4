d816c830e42f7b751c2c498274f465a077c58470
Aluriak <lucas.bourneuf@laposte.net>
2012-10-22 19:07:38 +0200

    Premières structures de données du moteur

--- modules/moteur/moteur.c
+++ modules/moteur/moteur.c
@@ -1,1 +1,47 @@
 #include "moteur.h"
+
+
+
+
+/*
+ * STRUCT T_CASE
+ */
+// STRUCT T_CASE: initialisation de la structure
+void t_case_init(t_case* t_case, int x, int y) {
+    t_case->crd.x = x;
+    t_case->crd.y = y;
+    t_case->joueurPieceCreuse = -1;
+    t_case->joueurPiecePleine = -1;
+    t_case->p = VIDE;
+}
+
+
+
+/*
+ * STRUCT T_JEU
+ */
+// STRUCT T_JEU: initialisation de la structure. Retourne false en cas d'erreur
+bool t_jeu_init(t_jeu *jeu, short nbjoueurs) {
+    if(nbjoueurs < 2) return false;
+    jeu->nbJoueur = nbjoueurs;
+    // allocation de la liste de joueur
+    jeu->listeJoueur = malloc(nbjoueurs * sizeof(t_joueur));
+    if(jeu->listeJoueur) {
+	FLUX_ERREUR("MODULE MOTEUR", "Allocation mémoire échouée à l'initialisation de la table de joueur");
+	return false;
+    }
+    // allocation du plateau de jeu
+    jeu->nbCaseX = 7 + (nbjoueurs-2);
+    jeu->nbCaseY = 6 + (nbjoueurs-2);
+    // TODO
+}
+
+
+void t_jeu_free(t_jeu* jeu) {
+    // libération des joueurs
+    free(jeu->listeJoueur);
+    // libération du jeu
+
+}
+
+
--- modules/moteur/moteur.h
+++ modules/moteur/moteur.h
@@ -11,12 +11,48 @@
 /*
  * PREDECLARATIONS
  */
+// structure de joueur
+typedef struct {
+    int point; // nombre de points pour cette partie
+    short idJ; // id reconnaissant le joueur
+    // il existe 125 id de joueurs. Soit 125 joueurs au max 
+    // zéro et les négatifs sont utilisés pour l'absence de joueur
+} t_joueur;
+
+
+// Coordonnées entières
+typedef struct {
+    int x;
+    int y;
+} coord;
+
+
+// Case du plateau de jeu
+typedef struct {
+    coord crd; // coordonnées de la case dans le jeu
+    short joueurPieceCreuse; // joueur contrôlant la pièce creuse, ou -1
+    short joueurPiecePleine; // joueur contrôlant la pièce pleine, ou -1
+} t_case;
+
+
+// structure de jeu
+typedef struct {
+    t_case** plateau; // matrice de t_case
+    int nbCaseX; // nombre de cases en X
+    int nbCaseY; // nombre de cases en Y
+    t_joueur listeJoueur[]; // liste des joueurs
+    short nbJoueur; // nombre de joueurs listés
+} t_jeu;
 
 
 
 /*
  * PROTOTYPES
  */
+// STRUCT T_CASE
+    void t_case_init(t_case* t_case, int x, int y); // initialisation de la structure
+
+
 
 
 
