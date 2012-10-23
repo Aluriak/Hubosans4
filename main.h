66addd139ede021c0f0fc4eb4f93fada0c162550
Aluriak <lucas.bourneuf@laposte.net>
2012-10-23 15:58:48 +0200

    Inclusion des modules systeme, graphique et moteur

--- main.h
+++ main.h
@@ -7,6 +7,10 @@
  */
 #include <stdlib.h>
 #include <stdio.h>
+// modules
+#include "modules/systeme/systeme.h"
+#include "modules/moteur/moteur.h"
+#include "modules/graphique/TERM/graphique_TERM.h"
 
 
 
