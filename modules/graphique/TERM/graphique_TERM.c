a4e305bb351261bd964e39709c7b95ebf7bc8d8f
Aluriak <lucas.bourneuf@laposte.net>
2012-10-23 16:02:31 +0200

    Fonctions TERM_clear et TERM_confColor

--- modules/graphique/TERM/graphique_TERM.c
+++ modules/graphique/TERM/graphique_TERM.c
@@ -1,27 +1,50 @@
 #include "graphique_TERM.h"
 
 
+/*
+ * TERM CLEAR
+ */
+// efface le contenu du terminal
+void TERM_clear() {
+    // directive comprise comme effaçage de l'écran
+    printf("\033[H\033[2J");
+}
+
+/*
+ * TERM CONFIGURE COLOR
+ */
+// configure la couleur de texte terminal
+// 	Envoyer "0" pour réinitialiser
+void TERM_confColor(char* clr) {
+    printf("\033[%sm", clr);
+}
 
 
 
 /*
- * AFFICHER JEU TERM
+ * TERM AFFICHER JEU
  */
 // affiche le jeu dans le terminal
-void afficherJeu_TERM(t_jeu* jeu) {
+void TERM_afficherJeu(t_jeu* jeu) {
+    // on efface l'écran
+    TERM_clear();
+    // on affiche de nouveau le plateau
     printf("   ");
     int i = 0, j = 0, val1, val2;
     char cCreuse, cPleine;
+    // pour chaque ligne
     for(i = 0; i < jeu->nbCaseX; i++) {
 	printf("|");
+	// pour chaque colonne
 	for(j = 0; j < jeu->nbCaseY; j++) {
-	    val1 = jeu->plateau[i][j]->pieceCreuse;
-	    val2 = jeu->plateau[i][j]->piecePleine;
+	    // récupération des idJoueurs 
+	    val1 = jeu->plateau[i][j].joueurPieceCreuse;
+	    val2 = jeu->plateau[i][j].joueurPiecePleine;
 	    if(val1 != -1)
 		cCreuse = 'O';
 	    if(val2 != -1)
 		cPleine = '.';
-	    if(jeu->plateau[i][j]->piece == BLOQUANTE)
+	    if(jeu->plateau[i][j].typePiece == BLOQUANTE)
 		cCreuse = cPleine = 'X';
 	    printf("|%c%c|", cCreuse, cPleine);
 	}
@@ -32,3 +55,6 @@ void afficherJeu_TERM(t_jeu* jeu) {
 	printf("*");
     printf("\n");
 }
+
+
+
