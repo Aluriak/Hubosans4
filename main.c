547946a3f8a3d2cdc12bd35004f01f4ec1bdc10c
Aluriak <lucas.bourneuf@laposte.net>
2012-11-03 16:49:23 +0100

    Chemin vers modules corrigé

--- main.c
+++ main.c
@@ -12,6 +12,8 @@ int main(int argc, char* argv[]) {
     t_jeu *jeu = t_jeu_init(2, 1); // 2 joueurs, dont 1 IA
     if(jeu == NULL) return false;
     TERM_afficherJeu(jeu);
+
+    // libération du jeu
     free(jeu);
     //	DEBUG 	*/
     return EXIT_SUCCESS;
