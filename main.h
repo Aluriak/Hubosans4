e2a9553ae66ea3caae10c705593773a6b9e3a8ea
Aluriak <lucas.bourneuf@laposte.net>
2012-11-03 17:22:38 +0100

    Adaptation à la nouvelle architecture

--- main.h
+++ main.h
@@ -6,9 +6,9 @@
  * BIBLIOTHEQUES
  */
 // modules
-#include "modules/systeme/systeme.h"
-#include "modules/moteur/moteur.h"
-#include "modules/graphique/TERM/graphique_TERM.h"
+#include "systeme/systeme.h"
+#include "moteur/moteur.h"
+#include "graphique/TERM/graphique_TERM.h"
 
 
 
