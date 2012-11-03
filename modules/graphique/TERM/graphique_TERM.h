b9927cb845ca5995c540f9d2e311e1e134301e81
Aluriak <lucas.bourneuf@laposte.net>
2012-11-03 16:19:36 +0100

    Ajout prototypes et rangement des fonctions selon source

--- modules/graphique/TERM/graphique_TERM.h
+++ modules/graphique/TERM/graphique_TERM.h
@@ -24,6 +24,7 @@
     void TERM_afficherJeu(t_jeu* jeu); // affiche le jeu dans le terminal
     // procédures internes
 	void TERM_afficherEnTete(t_jeu* jeu); // Affiche l'en-tête du jeu, contenant instructions, indications, et première partie du plateau de jeu
+	void TERM_afficherPlateau(t_jeu* jeu); // Affiche le plateau de jeu
 	void TERM_afficherCase(t_jeu* jeu, int i, int j); // affiche la case du jeu aux coordonnées (i;j), avec formatage couleur
 
 #endif
