270e13ce1304265aeb682d8b94b60e32434ea762
Aluriak <lucas.bourneuf@laposte.net>
2012-11-04 11:45:07 +0100

    FLUX_ERREUR vers fichier spécifique, randN() corrigée

--- systeme/systeme.c
+++ systeme/systeme.c
@@ -8,7 +8,9 @@
  */
 // affiche dans stderr l'erreur envoyée
 void FLUX_ERREUR(char* source, char* erreur) {
-    fprintf(stderr, "%s: %s", source, erreur);
+    FILE* fileErr = fopen(FILE_ERR, "wa");
+    fprintf(fileErr, "%s: %s", source, erreur);
+    fclose(fileErr);
 }
 
 
@@ -21,7 +23,7 @@ void FLUX_ERREUR(char* source, char* erreur) {
  */
 // renvois un nombre aléatoire entre 0 et N-1 (N > 0)
 int randN(int N) {
-    return (int) (rand() / (double)RAND_MAX * (N-1));
+    return (int) (rand() / (double)RAND_MAX * (N));
 }
 
 
