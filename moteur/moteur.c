3b4bdcb8527fc6f3f2ddd867480e1f8441c88b69
thomas <thomas.huba@gmail.com>
2012-11-12 18:15:15 +0100

    change a few change

--- moteur/moteur.c
+++ moteur/moteur.c
@@ -7,8 +7,9 @@
  */
 // effectue le tour suivant du jeu, et renvois l'adresse du joueur 
 // 	ayant fait un puissance 4 ce tour ou NULL.
-t_joueur* t_jeu_tourSuivant(t_jeu* jeu) {
-    return NULL;
+t_joueur* t_jeu_tourSuivant(t_jeu* jeu, t_action action ) {
+		
+	return NULL;
 }
 
 
