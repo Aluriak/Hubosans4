ae91bef0cb6e12797cabce47bb04e68a29d5efd4
thomas <thomas.huba@gmail.com>
2012-11-26 17:07:16 +0100

    del sync_git_to_redmine.sh && add str2int in system.c

--- systeme/systeme.c
+++ systeme/systeme.c
@@ -61,4 +61,31 @@ int tab_max(int* tab, int T) {
 }
 
 
-
+/*
+ * STR2INT
+ */
+//renvois un entier positif à partir d'une chaine de caractère ne contenant que des entiers positifs
+//Valeurs ASCII :
+//	dec chr   dec chr
+//	----------------
+//	48 : 0	| 53 : 5
+//	49 : 1	| 54 : 6
+//	50 : 2	| 55 : 7
+//	51 : 3	| 56 : 8
+//	52 : 4	| 57 : 9
+//
+int str2int(char * str, int nb_carac)
+{
+	int i = 0; // itérateur de boucle
+	int resultat = 0; // Variable retournée contenant la valeur de str
+	char tmp; // Variable tampon
+	// Tant que tmp n'a pas lu tout les chiffres
+	for(i=0;i<nb_carac;i++)
+	{
+		printf("tour %i: str[i] = %c\n", i, str[i]);
+		tmp=str[i]; // On récupère la valeur de str[i] pour traitement
+		tmp -= 48; // On retire 48 pour obtenir la valeur en int
+		resultat += tmp*pow(10, nb_carac-1-i); 
+	}
+	return resultat;
+}
