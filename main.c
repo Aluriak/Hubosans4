454e55b597fcfdacc4cc8efe2b608f538af7ce02
Aluriak <lucas.bourneuf@laposte.net>
2012-12-05 10:04:42 +0100

    Correction ppur compilation

--- main.c
+++ main.c
@@ -12,10 +12,11 @@ int main(int argc, char* argv[]) {
     main_init();
 
     // initialisations
+    t_jeu *jeu = NULL; // initialisé par l'interface graphique
     t_joueur* gagnant = NULL;
     t_action action;
     bool sdl = false;
-    SDL_Surface* ecran = NULL; // écran de jeu
+    //SDL_Surface* ecran = NULL; // écran de jeu
 
     // si le premier argument est "-g"
     if(argc > 1 && (strcmp(argv[1],"-g") == 0 || 
@@ -26,35 +27,36 @@ int main(int argc, char* argv[]) {
 
     // Menu principal
     if(sdl) {
-	//t_jeu* jeu = SDL_AfficherMenu(ecran);
+	//jeu = SDL_AfficherMenu(ecran);
 	//SDL_afficherJeu(&jeu, ecran);
     }
     else {
-	t_jeu* jeu = TERM_AfficherMenu();
-	TERM_afficherJeu(&jeu);
+	jeu = TERM_afficherMenu();
+	TERM_afficherJeu(jeu);
     }
 
     // jeu
     while(gagnant == NULL) {
 	if(sdl) {
-	    //SDL_afficherJeu(&jeu, ecran);
-	    //action = SDL_entreeUtilisateur(&jeu, ecran);
+	    //SDL_afficherJeu(jeu, ecran);
+	    //action = SDL_entreeUtilisateur(jeu, ecran);
 	}
 	else {
-	    TERM_afficherJeu(&jeu);
-	    action = TERM_entreeUtilisateur(&jeu);
+	    TERM_afficherJeu(jeu);
+	    action = TERM_entreeUtilisateur(jeu);
 	}
-	gagnant = MOTEUR_tourSuivant(&jeu, action);
+	gagnant = MOTEUR_tourSuivant(jeu, action);
     }
     // arrivé ici, il y a puissance 4
-=======
+    /*
     if(sdl)
 	//SDL_afficherFinDeJeu(&jeu, ecran);
     else
 	//TERM_afficherFinDeJeu(&jeu);
+    // */
 
     // libération du jeu
-    t_jeu_free(&jeu);
+    t_jeu_free(jeu);
     // TODO: libérations et désinitialisations SDL
     return EXIT_SUCCESS;
 }
