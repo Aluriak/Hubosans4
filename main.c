6052d818bd97415ed621adbbcfaed3c0e70019e2
Aluriak <lucas.bourneuf@laposte.net>
2012-12-11 14:43:02 +0100

    Plus d'appels à l'IA

--- main.c
+++ main.c
@@ -43,9 +43,11 @@ int main(int argc, char* argv[]) {
 	else {
 	    TERM_afficherJeu(jeu);
 	    t_joueur* oya = t_jeu_getOya(jeu);
+	    /*
 	    if(oya->IA == true)
 		action = IA_effectuerTour(jeu, oya);
 	    else
+	    // */
 		action = TERM_entreeUtilisateur(jeu);
 	}
 	gagnant = MOTEUR_tourSuivant(jeu, action);
