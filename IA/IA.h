981af4bd33e1019c86ebaeac3fa652db829d5a3e
Aluriak <lucas.bourneuf@laposte.net>
2012-12-16 12:19:09 +0100

    Algorithme minimax terminé

--- IA/IA.h
+++ IA/IA.h
@@ -12,23 +12,17 @@
 /*
  * PREDECLARATIONS
  */
-// structure utilisée pour manipuler les priorités
-typedef struct {
-    int pleine; // priorité pour la pièce Pleine
-    int creuse; // priorité pour la pièce Creuse
-    int bloquante; // priorité pour la pièce Bloquante
-} t_priorite;
 
 
 
 /*
  * PROTOTYPES
  */
-// IA.c
-    t_action IA_effectuerTour(t_jeu *jeu, t_joueur* joueur); // effectue le tour de jeu du joueur pointé. Retourne l'action effectuée par l'IA
-    t_action IA_creerAction(t_jeu* jeu, t_joueur* joueur); // détermination de l'action à effectuer selon algorithme minimax
+// FILE: IA.c
+    t_action IA_effectuerTour(t_jeu *jeu); // effectue le tour de jeu du joueur pointé. Retourne l'action effectuée par l'IA
+    int IA_minimax(t_jeu*, int, int, t_action*, t_action);// étudie récursivement le plateau de jeu, et retourne la priorité. 
 
-// heuristique.c
+// FILE: heuristique.c
     int IA_h(t_jeu* jeu); // renvois le gain de la situation du jeu envoyé pour l'oya (between 1 and 10) ou -1 en cas d'erreur
 
 #endif
