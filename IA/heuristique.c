c09b82ed7f11aa10036bf2305e22b04138fd2d7a
Aluriak <lucas.bourneuf@laposte.net>
2013-01-09 01:10:13 +0100

    Avancement ?

--- IA/heuristique.c
+++ IA/heuristique.c
@@ -5,106 +5,142 @@
 /*
  * IA HEURISTIQUE
  */
-// renvois le gain de la situation du jeu envoyé pour l'oya (between 0 and 100)
-// ou -1 en cas d'erreur
-// règles de priorités :
-//      - l'oya a 3 pièces alignées     : 90
-//      - l'oya a 2 pièces alignées     : 70
-//      - l'oya a 1 pièce alignée       : 50
-//      - un autre a 1 pièce alignée    : 40
-//      - un autre a 2 pièces alignées  : 20
-//      - un autre a 3 pièces alignées  : 0
-// On garde évidemment la priorité la plus haute, on teste donc pour 4, puis 3, 
-//  puis 2, puis 1 pièce.
+// renvois le gain de la situation du jeu envoyé pour l'oya (entre 0 et 100)
 //  plus une priorité est basse, plus elle est indésirable
 int IA_h(t_jeu* jeu, int idIA) {
-    // INITIALISATIONS
-    int i = 0, j = 0, k = 0; // itérateurs de boucle
-    coord coordCase = {-1,-1}; // coordonnées de la case étudiée
-    // nombre maximum de pièces du même joueur alignées trouvé jusque là
-    // tableau dont l'index est l'id des joueur. Chaque joueur, avec son id, 
-    // accède au nombre maximum de pièce qu'il a alignées avec suffisemment
-    //  de cases vide pour faire un puissance 4
-    int *nb_pieceAligneeMax = malloc(jeu->nbJoueur*sizeof(int)); 
-        if(nb_pieceAligneeMax == NULL) { // gestion d'erreur
-            FLUX_ERREUR("IA:", "malloc pour idJ* dans l'heuristique échouée");
-            exit(EXIT_FAILURE);
-        }
-    // mise à 0 de toutes les cases
-    for(i = 0; i < jeu->nbJoueur; i++)
-        nb_pieceAligneeMax[i] = 0;
-    // priorité à retourner :
-    int priorite = -1;
-    // nombre de pièces maximum alignées :
-    int nb_pieceMax = 0;
+    // INITIALISATION
+    int i, y; // itérateurs de boucles
+    int joueur; // id du joueur actuellement considéré
+    int nb_piece_alignees; // empalcement occupé par le joueu
+    int nb_vide; // emplacements vides
+    int alignMax = 0; // alignement de pièces maximum pour trois pièces
+    int joueur_max = -1; // joueur alignant le plus de pièce (idIA si égalité)
+    t_case casac; // case étudiée
 
-
-
-    // DÉTERMINATION DE LA PRIORITÉ
-    // on détermine le nombre de pièces max alignées pour chaque joueur
-    // Pour chaque case
-        // pour chaque joueur
-                // on utilise la fonction suivante, pour chaque joueur
-                // int MOTEUR_test_puissance4(jeu, coordCase, idJoueur);
-                // Renvois le nombre de cases alignées par le joueur par rapport
-                //  à cette case
-                // La k-ième case du tableau de priorité est égale à
-                //      max(résultat fonction, valeur de la case du tableau)
-    // pour chaque case du jeu (on parcours de haut en bas, de gauche à droite)
-    for(i = jeu->nbCaseX-1; i >= 0; i--) {
-        for(j = jeu->nbCaseY-1; j >= 0; j--) {
-            coordCase.x = i;
-            coordCase.y = j;
-            // pour chaque joueur 
-            for(k = jeu->nbJoueur-1; k >= 0; k--) {
-                // on prend le nombre de case alignées par rapport à cette case
-                nb_pieceMax = MOTEUR_test_puissance4(jeu, coordCase, k);
-                // on prend le nombre max de pièces alignées par le joueur
-                nb_pieceAligneeMax[k] = max(nb_pieceAligneeMax[k], nb_pieceMax);
-            }
+    // TRAITEMENT
+    for(joueur = 0; joueur < jeu->nbJoueur; joueur++) 
+    // pour chaque ligne
+    for(y = 0; y < jeu->nbCaseY; y++) {
+        nb_piece_alignees = nb_vide = 0;
+        // parcours de la ligne
+        for(i = 0; i < jeu->nbCaseX; i++) {
+                casac = jeu->plateau[y][i]; // case actuellement étudiée
+                // si la case appartient au moins en partie à l'IA
+                if(IA_caseAppartientA(joueur, casac))
+                    nb_piece_alignees++;
+                // si elle est vide
+                else if(IA_caseVide(casac))
+                    nb_vide++;
+                // sinon, c'est que la case est complètement bloquée par qqn d'autre
+                else {
+                    // on comptabilise le nombre de pièces si ya un espace vide
+                    // et assez de place pour 4 pièces ou plus
+                    // sinon c'est qu'on ne peut pas aligner plus de pièces
+                    if(nb_vide > 0 && nb_vide+nb_piece_alignees >= 4) {
+                        // si le nouvel alignement est plus grand, ou aussi grand
+                        if(nb_piece_alignees >= alignMax) {
+                            // alignMax au plus haut
+                            alignMax = max(alignMax, nb_piece_alignees);
+                            // et le joueur_max deviens le joueur actuel
+                            joueur_max = joueur;
+                        }
+                    }
+                    // compteurs réinitialisés
+                    nb_vide = nb_piece_alignees = 0;
+                }
         }
     }
-            
-
-    // on a maintenant le nombre maximum de pièce alignées pour chaque joueur
-    // on parcours les joueurs, et on prend le nombre de pièces max alignées
-    // pour chaque joueur
-    nb_pieceMax = tab_max(nb_pieceAligneeMax, jeu->nbJoueur);
-    // maintenant, on regarde combiend e joueurs ont atteint ce nombre de pièces
-    // si il y en plus d'un, c'est mauvais signe !
-    if(tab_count(nb_pieceAligneeMax, jeu->nbJoueur, nb_pieceMax) == 1 && 
-            nb_pieceAligneeMax[idIA] == nb_pieceMax) {
-        // on définit la priorité renvoyée
-        if(nb_pieceMax >= 3)
-            priorite = 100;
-        else if(nb_pieceMax == 2)
-            priorite = 70;
-        else if(nb_pieceMax == 1)
-            priorite = 60;
-        else // cas où il y a 0 pions alignés
-            priorite = 50; // priorité moyenne
+    // pour chaque colonne
+    for(i = 0; i < jeu->nbCaseX; i++) {
+        nb_piece_alignees = nb_vide = 0;
+        // parcours de la colonne (de haut en bas)
+        for(y = 0; y < jeu->nbCaseY; y++) {
+                casac = jeu->plateau[y][i]; // case actuellement étudiée
+                // si la case appartient au moins en partie à l'IA
+                if(IA_caseAppartientA(joueur, casac))
+                    nb_piece_alignees++;
+                // si elle est vide
+                else if(IA_caseVide(casac))
+                    nb_vide++;
+                // sinon, c'est que la case est complètement bloquée par qqn d'autre
+                else {
+                    // on comptabilise le nombre de pièces si ya un espace vide
+                    // et assez de place pour 4 pièces ou plus
+                    // sinon c'est qu'on ne peut pas aligner plus de pièces
+                    if(nb_vide > 0 && nb_vide+nb_piece_alignees >= 4) {
+                        // si le nouvel alignement est plus grand, ou aussi grand
+                        if(nb_piece_alignees >= alignMax) {
+                            // alignMax au plus haut
+                            alignMax = max(alignMax, nb_piece_alignees);
+                            // et le joueur_max deviens le joueur actuel
+                            joueur_max = joueur;
+                        }
+                    }
+                    // compteurs réinitialisés
+                    nb_vide = nb_piece_alignees = 0;
+                }
+        }
     }
-    else { // c'est un adversaire qui a le nombre de pièce max !
-        // on définit la priorité renvoyée
-        if(nb_pieceMax >= 3)
-            priorite = 0;
-        else if(nb_pieceMax == 2)
-            priorite = 20;
-        else if(nb_pieceMax == 1)
-            priorite = 40;
-        else 
-            priorite = 50;
+
+
+
+printf("alignMax = %i\n", alignMax);
+    if(joueur_max == idIA) {
+        if(alignMax >= 3)
+            return 90;
+        else if(alignMax == 2)
+            return 70;
+        else if(alignMax == 1)
+            return 60;
+        else
+            return 50;
+    }
+    // c'est un autre joueur qui aligne le plus de pièces
+    else {
+        if(alignMax >= 3)
+            return 0;
+        else if(alignMax == 2)
+            return 20;
+        else if(alignMax == 1)
+            return 40;
+        else
+            return 50;
     }
 
-    // RETOUR DE LA PRIORITÉ
-    // libération
-    free(nb_pieceAligneeMax);
-    // return
-    /*printf("priorite = %d (J%d)\n", priorite, jeu->oya);*/
-    /*TERM_afficherJeu(jeu);*/
-    return priorite;    
+    // END IA_h()
 }
 
 
+
+
+
+/*
+ * IA: CASE APPARTIENT A
+ */
+// renvosi vrai si la case appartient, en partie au moins, au joueur envoyé
+bool IA_caseAppartientA(int idJ, t_case casac) {
+    return (casac.joueurPiecePleine == idJ || casac.joueurPieceCreuse == idJ);
+}
+
+
+
+/*
+ * IA: CASE VIDE
+ */
+// renvois vrai si la case envoyée est vide
+bool IA_caseVide(t_case casac) {
+    return (casac.joueurPiecePleine == -1 && casac.joueurPieceCreuse == -1);
+}
+
+
+
+/*
+ * IA: CASE JOUABLE
+ */
+// renvois vrai si la case envoyée est encore jouable (il y a au moins un empalcement libre
+// NB: aucune vérification si une pièce peut y être déposée !
+bool IA_caseJouable(t_case casac) {
+    return (casac.joueurPiecePleine == -1 || casac.joueurPieceCreuse == -1);
+}
 
 
